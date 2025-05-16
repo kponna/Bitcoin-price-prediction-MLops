118551aff5c570da6bd5deb6f14c3cedb1965d17
kponna <ponnakrishnaveni76@gmail.com>
2025-05-16 04:08:15 +0000

    Connecting the crypto api

---
+++ data_manager/crypto_api.py
@@ -0,0 +1,37 @@
+import requests
+from src.logging.logger import logging
+import pandas as pd
+import os
+from dotenv import load_dotenv 
+load_dotenv()
+
+def fetch_crypto_data(api_uri): 
+    response = requests.get(
+        api_uri,
+        params={
+            "market": "cadli",
+            "instrument": "BTC-USD",
+            "limit": 5000,
+            "aggregate": 1,
+            "fill": "true",
+            "apply_mapping": "true",
+            "response_format": "JSON"
+        },
+        headers={"Content-type": "application/json; charset=UTF-8"}
+    )
+
+    if response.status_code == 200:
+        print('API Connection Successful! \nFetching the data...') 
+        data = response.json()
+        data_list = data.get('Data', []) 
+        df = pd.DataFrame(data_list) 
+        df['DATE'] = pd.to_datetime(df['TIMESTAMP'], unit='s') 
+        return df 
+    else:
+        raise Exception(f"API Error: {response.status_code} - {response.text}")
+
+
+if __name__ == "__main__":
+    API_URI = os.getenv("API_URI")
+    df = fetch_crypto_data(api_uri = API_URI)
+    logging.info(f"df : {df.head()}")
---
+++ src/data_ingestor.py
@@ -0,0 +1,30 @@
+import os
+import logging
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import pandas as pd
+from src.logging import logger
+from src.exception_handling.custom_exception import BitCoinException
+
+load_dotenv() 
+MONGO_URI = os.getenv("MONGO_URI")
+
+def fetch_data_from_mongodb(collection_name:str, database_name:str):  
+    client = MongoClient(MONGO_URI)
+    db = client[database_name]  
+    collection = db[collection_name]    
+    try:
+        logging.info(f"Fetching data from MongoDB collection: {collection_name}...")
+        data = list(collection.find())  
+
+        if not data:
+            logging.info("No data found in the MongoDB collection.")
+             
+        df = pd.DataFrame(data) 
+        if '_id' in df.columns:
+            df = df.drop(columns=['_id']) 
+        logging.info("Data successfully fetched and converted to a DataFrame!")
+        return df 
+    except Exception as e:
+        logging.error(f"An error occurred while fetching data: {e}")
+        raise e  
